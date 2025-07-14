45da1a1f4d334bfab161f220c8fddf1024000988
DarkLotus <darklotus@gmail.com>
2025-07-14 14:01:20 -0500

    Update waxing for thatch blocks.

--- src/main/java/com/lithiumcraft/stuff_and_things/item/ModItems.java
+++ src/main/java/com/lithiumcraft/stuff_and_things/item/ModItems.java
@@ -1,17 +1,10 @@
 package com.lithiumcraft.stuff_and_things.item;
 
 import com.lithiumcraft.stuff_and_things.StuffAndThings;
-import com.lithiumcraft.stuff_and_things.block.ModBlocks;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
-import java.util.Map;
-import java.util.function.Supplier;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StuffAndThings.MOD_ID);
